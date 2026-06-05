4e124df4757c528383be2dc13c63a601bb3862d5
BogdanPysar <59173880+PiLoT-P@users.noreply.github.com>
2026-06-05 10:51:31 +0300

    ✅Update loader and DataTable text

--- packages/ui/src/components/loader/Loader.tsx
+++ packages/ui/src/components/loader/Loader.tsx
@@ -4,14 +4,16 @@ import s from './Loader.module.scss';
 export interface LoaderProps {
   size?: number;
   color?: string;
+  style?: React.CSSProperties;
 }
 
 export const Loader: FC<LoaderProps> = ({
   size = 40,
-  color = 'var(--text-color)', 
+  color = 'var(--text-color)',
+  style,
 }) => {
   return (
-    <div className={s.loader_container}>
+    <div className={s.loader_container} style={style}>
       <svg
         className={s.loader}
         viewBox="22 22 44 44"
